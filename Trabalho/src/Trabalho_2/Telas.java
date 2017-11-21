649b13cac6761ba2768bdf74d0c6eff3b70766c3
Valdecir <valdecirvjunior@gmail.com>
2017-11-21 15:04:42 -0200

    ajustes

--- Trabalho/src/Trabalho_2/Telas.java
+++ Trabalho/src/Trabalho_2/Telas.java
@@ -36,9 +36,33 @@ public class Telas extends javax.swing.JFrame {
      * Creates new form Telas
      */
     CaixaDeTexto caixa = new CaixaDeTexto();
+    
     public Telas() {
         initComponents();
+    }
+    
+    public void atualizaLetras(){
+        int vog = 0, cons = 0, espc = 0;
+
+        String frase = caixa.Texto.toLowerCase();
+        for(int i = 0; i < frase.length(); i++){
+            char c = frase.charAt(i);
+            if(c == 32){
+                espc++;
+                continue;
+            }
+            if(c == 97 | c == 101 | c == 105 | c == 111 | c == 117){
+                vog++;
+                continue;
+            }
+            if(c >= 98 && c <= 122) cons++;
+        }
+
+        qntVogais.setText(Integer.toString(vog));
+        qntConsoantes.setText(Integer.toString(cons));
+        qntEspaco.setText(Integer.toString(espc));
         
+        vog = 0; cons = 0; espc = 0;
     }
     /**
      * This method is called from within the constructor to initialize the form.
@@ -51,8 +75,8 @@ public class Telas extends javax.swing.JFrame {
 
         jFrame1 = new javax.swing.JFrame();
         jFrame2 = new javax.swing.JFrame();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        lerArquivo = new javax.swing.JButton();
+        salvarArquivo = new javax.swing.JButton();
         jScrollPane3 = new javax.swing.JScrollPane();
         Tela1 = new javax.swing.JTextArea();
         jScrollPane2 = new javax.swing.JScrollPane();
@@ -63,6 +87,9 @@ public class Telas extends javax.swing.JFrame {
         qntConsoantes = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         qntEspaco = new javax.swing.JLabel();
+        limparArquivo = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
 
         javax.swing.GroupLayout jFrame1Layout = new javax.swing.GroupLayout(jFrame1.getContentPane());
         jFrame1.getContentPane().setLayout(jFrame1Layout);
@@ -88,17 +115,17 @@ public class Telas extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("Ler Arquivo");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        lerArquivo.setText("Ler Arquivo");
+        lerArquivo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                lerArquivoActionPerformed(evt);
             }
         });
 
-        jButton2.setText("Salvar Arquivo");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        salvarArquivo.setText("Salvar Arquivo");
+        salvarArquivo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                salvarArquivoActionPerformed(evt);
             }
         });
 
@@ -116,6 +143,15 @@ public class Telas extends javax.swing.JFrame {
 
         jLabel2.setText("Espaços:");
 
+        limparArquivo.setText("Limpar Arquivo");
+        limparArquivo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                limparArquivoActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setText("Arquivo:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -125,83 +161,95 @@ public class Telas extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(51, 51, 51)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel3)
-                                    .addComponent(jLabel1))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(qntVogais, javax.swing.GroupLayout.DEFAULT_SIZE, 143, Short.MAX_VALUE)
-                                    .addComponent(qntConsoantes, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel2)
-                                .addGap(18, 18, 18)
-                                .addComponent(qntEspaco, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 72, Short.MAX_VALUE))
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(qntEspaco, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(qntVogais, javax.swing.GroupLayout.DEFAULT_SIZE, 143, Short.MAX_VALUE)
+                                .addComponent(qntConsoantes, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(jScrollPane2)
-                        .addGap(18, 18, 18)))
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 342, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton2)
-                        .addGap(18, 18, 18)
-                        .addComponent(jButton1))
-                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 326, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap())
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 23, Short.MAX_VALUE)
+                        .addComponent(lerArquivo)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(salvarArquivo)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(limparArquivo)
+                        .addGap(20, 20, 20))
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane3)
+                        .addContainerGap())))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(295, 295, 295)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jScrollPane2)
-                    .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 468, Short.MAX_VALUE))
+                .addGap(13, 13, 13)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 441, Short.MAX_VALUE)
+                            .addComponent(jScrollPane2))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel1)
                             .addComponent(qntVogais, javax.swing.GroupLayout.PREFERRED_SIZE, 13, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(1, 1, 1)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(qntConsoantes, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addGap(2, 2, 2)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel2)
-                            .addComponent(qntEspaco, javax.swing.GroupLayout.PREFERRED_SIZE, 11, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(63, 63, 63))
+                            .addComponent(qntConsoantes, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(15, 15, 15)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jButton2)
-                            .addComponent(jButton1))
-                        .addGap(0, 0, Short.MAX_VALUE))))
+                            .addComponent(lerArquivo)
+                            .addComponent(salvarArquivo)
+                            .addComponent(limparArquivo))))
+                .addGap(2, 2, 2)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel2)
+                    .addComponent(qntEspaco, javax.swing.GroupLayout.PREFERRED_SIZE, 11, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(15, 15, 15))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void salvarArquivoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_salvarArquivoActionPerformed
         caixa.Texto = Tela1.getText();
         File arq = new File(caixa.Diretorio);
+        
         try {
             FileWriter fw = new FileWriter(arq,true);
             PrintWriter pw = new PrintWriter(fw);
-            pw.println("");
-            //pw.println(caixa.Texto);
+            pw.write(""); 
+            pw.flush();
+            pw.write(caixa.Texto);
             pw.flush();
             pw.close();
         } catch (IOException ex) {
             Logger.getLogger(Telas.class.getName()).log(Level.SEVERE, null, ex);
         }
 	Tela2.setText(caixa.Texto);
+        atualizaLetras();
+    }//GEN-LAST:event_salvarArquivoActionPerformed
 
-
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void lerArquivoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_lerArquivoActionPerformed
         JFileChooser chooser = new JFileChooser();
         if (chooser.showSaveDialog(null) != JFileChooser.APPROVE_OPTION)
             return;
@@ -218,36 +266,36 @@ public class Telas extends javax.swing.JFrame {
             while ( (s = br.readLine()) != null ){
 		Tela1.append(s + "\n");
             }
-            caixa.Texto = Tela1.getText();
+            caixa.Texto = Tela2.getText();
             caixa.Nome = file.getName();
             caixa.Diretorio = file.getCanonicalPath();
             
         } catch(Exception x){ 
             System.out.println(x.getMessage()); 
-        }    
-        
-        int vog = 0, cons = 0, espc = 0;
-
-        String frase = caixa.Texto.toLowerCase();
-        for(int i = 0; i < frase.length(); i++){
-            char c = frase.charAt(i);
-            if(c == 32){
-                espc++;
-                continue;
-            }
-            if(c == 97 | c == 101 | c == 105 | c == 111 | c == 117){
-                vog++;
-                continue;
-            }
-            if(c >= 98 && c <= 122) cons++;
         }
-
-        qntVogais.setText(Integer.toString(vog));
-        qntConsoantes.setText(Integer.toString(cons));
-        qntEspaco.setText(Integer.toString(espc));
+        jLabel5.setText(caixa.Nome);
+        atualizaLetras();
         
-        vog = 0; cons = 0; espc = 0;
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_lerArquivoActionPerformed
+
+    private void limparArquivoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_limparArquivoActionPerformed
+        caixa.Texto = "";
+        File arq = new File(caixa.Diretorio);
+        
+        try {
+            FileWriter fw = new FileWriter(arq);
+            //PrintWriter pw = new PrintWriter(fw);
+            fw.write(""); 
+            fw.flush();
+            fw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Telas.class.getName()).log(Level.SEVERE, null, ex);
+        }
+	Tela2.setText(caixa.Texto);
+        Tela1.setText(caixa.Texto);
+        
+        atualizaLetras();
+    }//GEN-LAST:event_limparArquivoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -297,18 +345,6 @@ public class Telas extends javax.swing.JFrame {
                 JOptionPane.showMessageDialog(null, "Senha invalida");
             }
         } while (!senha.equals("123"));
-                
-                
-		
-        
-        
-                
-                
-                
-                
-        
-        
-        
         
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -344,17 +380,20 @@ public class Telas extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextArea Tela1;
     private javax.swing.JTextArea Tela2;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JFrame jFrame1;
     private javax.swing.JFrame jFrame2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JButton lerArquivo;
+    private javax.swing.JButton limparArquivo;
     private javax.swing.JLabel qntConsoantes;
     private javax.swing.JLabel qntEspaco;
     private javax.swing.JLabel qntVogais;
+    private javax.swing.JButton salvarArquivo;
     // End of variables declaration//GEN-END:variables
 }
